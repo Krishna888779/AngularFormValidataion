8f555517f52519b511b313a11c400cfed238e8b3
My Name <rohitkumar@sourcemash.com>
2023-11-06 13:58:04 +0530

    update code

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -17,17 +17,17 @@ Mobile:any
  ngOnInit(){
   debugger
     this.forms=new FormGroup({
-      name:new FormControl("Please enter your name",[Validators.required,Validators.minLength(4)]),
-      email:new FormControl("Please enter your email",[Validators.required,Validators.email,Validators.pattern("[[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}]")]),
-      mobile: new FormControl("Please enter your mobile",[Validators.required,Validators.maxLength(10),Validators.minLength(10),Validators.pattern("[6-9]{1}[0-9]{9}")]),
+      name:new FormControl(" ",[Validators.required,Validators.minLength(4)]),
+      email:new FormControl(" ",[Validators.required,Validators.email,Validators.pattern("[^@\s]+@[^@\s]+\.[^@\s]+")]),
+      mobile: new FormControl(" ",[Validators.required,Validators.maxLength(10),Validators.minLength(10),Validators.pattern("[6-9]{1}[0-9]{9}")]),
     })
  }
- get nameValidation(){return this.forms.get('name')}
+ get nameValidation(){ debugger;return this.forms.get('name')}
  get emailValidation(){return this.forms.get('email')}
  get mobileValidation(){return this.forms.get('mobile')}
  OnSumbit(){
   debugger
-  this.forms
+  this.forms.valid 
   this.Name=this.forms.value.name
   this.Email=this.forms.value.email
   this.Mobile=this.forms.value.mobile
